dfae25b6718504b9d91a5bfa463b9b15a8b59b3e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-11-21 18:25:46 +0800

    Revamped yearly stats through iframe

--- assets/ps-script.js
+++ assets/ps-script.js
@@ -5,37 +5,6 @@
 //  });
 //});
 
-// Tooltips
-const temp = document.createElement('template');
-temp.innerHTML =`
-  <style>
-  .tt-w {
-    position:absolute;
-    top:-40px;
-    left:-75px;
-    width:175px;
-    background: rgba(0,0,0, .75);
-    color: white;
-    padding: 8px 0;
-    border-radius: 5px;
-    font-size: 115%;
-  }  
-  </style>
-  <div class="tt-w">
-    <span style="display:flex;justify-content:center;"><strong id="title"></strong>&nbsp;artwork(s)&nbsp;submitted</span>
-  </div>
-`
-class toolTips extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({mode:'open'});
-    this.shadowRoot.appendChild(temp.content.cloneNode(true));
-    this.shadowRoot.getElementById('title').innerText = this.getAttribute('count');
-  };
-};
-
-window.customElements.define('aw-tooltip', toolTips);
-
 // Chrono buttons
 window.onload = () => {
   btn21 = document.getElementById('btn-21');
@@ -44,13 +13,5 @@ window.onload = () => {
     art21.classList.add('artwork-stats-21');
     art20.classList.remove('artwork-stats-20');
     console.log('mouse in');
-  }
-
-  btn20 = document.getElementById('btn-20');
-  art20 = document.getElementById('aw-20');
-  btn20.onmouseover = () => {
-    art21.classList.remove('artwork-stats-21');
-    art20.classList.add('artwork-stats-20');
-    console.log('mouse in1');
   }
 }
