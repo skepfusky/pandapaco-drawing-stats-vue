56511fd471e0d6788c59883ca27d907d844ee539
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-12-23 03:44:23 +0800

    Indents and renamed packages

--- src/js/paco-scripts.js
+++ src/js/paco-scripts.js
@@ -1,9 +1,9 @@
 // Import stuff
-const Chart = require('chart.js');
+const Chart = require("chart.js");
 
 // Init shadow DOM and custom HTML elements for header and footer for multiple pages
 // Custom header element
-const pacoHdrTemplate = document.createElement('template');
+const pacoHdrTemplate = document.createElement("template");
 pacoHdrTemplate.innerHTML = `
   <style>
     @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css");
@@ -24,14 +24,14 @@ class pacoHdr extends HTMLElement {
     super();
   }
   connectedCallback() {
-    const shadowRoot = this.attachShadow({mode: 'open'});
+    const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(pacoHdrTemplate.content);
   }
 }
-customElements.define('paco-hdr', pacoHdr);
+customElements.define("paco-hdr", pacoHdr);
 
 // Custom footer element
-const pacoFtrTemplate = document.createElement('template');
+const pacoFtrTemplate = document.createElement("template");
 pacoFtrTemplate.innerHTML = `
   <style>
     @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css");
@@ -131,9 +131,9 @@ class pacoFtr extends HTMLElement {
   constructor() {
     super();
   }
-  connectedCallback () {
+  connectedCallback() {
     const fontAwesome = document.querySelector('link[href*="font-awesome"]');
-    const shadowRoot = this.attachShadow({ mode: 'open' });
+    const shadowRoot = this.attachShadow({ mode: "open" });
 
     if (fontAwesome) {
       shadowRoot.appendChild(fontAwesome.cloneNode());
@@ -142,4 +142,4 @@ class pacoFtr extends HTMLElement {
     shadowRoot.appendChild(pacoFtrTemplate.content);
   }
 }
-customElements.define('paco-ftr', pacoFtr);
+customElements.define("paco-ftr", pacoFtr);
