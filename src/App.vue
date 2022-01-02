fc0b7b9cd7c32ff323109397dd8c874f28234a6f
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-01-02 11:51:13 +0800

    Current files pulled from the `main` branch

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <paco-navbar />
+  <router-view/>
+  <paco-footer />
+</template>
+
+<script>
+import PacoNavbar from "./components/PacoNavbar.vue";
+import PacoFooter from "./components/PacoFooter.vue";
+
+export default {
+  components: {
+    PacoNavbar, PacoFooter
+  }
+}
+</script>
+
+<style lang="scss">
+// All SCSS stylesheets are loaded all across Vue components
+</style>
---
+++ src/components/SpeciesCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <a class="species-card-header" href="#">
+      <img :src="require(`../img/${sImg}`)"/>
+      <!-- <img :src="`@/assets/img/header/${sImg}`" :alt="`${sAlt}`" :aria-label="`Picture of ${$sAlt}`"/> -->
+      <p>{{ sTitle }}</p>
+    </a>
+</template>
+
+<script>
+export default {
+  props: {
+    sImg: {type: String, default: "placeholder.png"},
+    // sAlt: {type: String,},
+    sTitle: {type: String, default: "Bruh"},
+  }
+}
+</script>
+
+<style lang="scss">
+.species-card-header {
+	@include p-flexy(center, center, column);
+	@include dropdown-element(-5px);
+	background: var(--bamboo-light-100);
+	color: var(--bamboo-light-400);
+	@include soft-corners;
+	padding-block: 12px;
+
+	&:hover {
+		background: var(--bamboo-light-200);
+    color: whitesmoke;
+	}
+
+	img {
+		width: 80px;
+		border-radius: 50%;
+		box-shadow: 0 0 12px rgba(black, 55%);
+	}
+
+	p {
+		margin-top: 12px;
+		font-size: 95%;
+	}
+}
+</style>
