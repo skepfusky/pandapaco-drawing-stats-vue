ffc5ed838cf0caec5c74e68e3bb7670cbd00b733
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-01-05 14:19:21 +0800

    added vue-i18n plugin

--- src/main.js
+++ src/main.js
@@ -7,13 +7,15 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { dom } from "@fortawesome/fontawesome-svg-core";
+import store from './store'
+import i18n from './i18n'
 
 library.add(fab);
 library.add(fas);
 library.add(far);
 dom.watch();
 
-const app = createApp(App);
+const app = createApp(App).use(i18n).use(store);
 app.component("font-awesome-icon", FontAwesomeIcon);
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(i18n).use(store).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,21 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@import "@/scss/styles.scss";`,
+        prependData: '@import "@/scss/styles.scss";'
       }
+    },
+    sourceMap: true
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: undefined,
+      fallbackLocale: undefined,
+      localeDir: undefined,
+      enableLegacy: undefined,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
     }
   }
 }
