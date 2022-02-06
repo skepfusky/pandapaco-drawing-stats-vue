fe41c596f5540dff704676a329b4357f6b3fde6e
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-02-06 22:06:44 +0800

    Re-run notebook script

--- data/paco-dataset-types.py
+++ data/paco-dataset-types.py
@@ -1,10 +1,10 @@
 # Provide dat good stuff
 import matplotlib.pyplot as plotDatGoodness
-import pandas as paco
+import pandas as pandapaco
 
 # Get 2021 compiled drawing data and filters digital and
 # traditional drawings
-paco2021 = paco.read_csv("data/paco-drawings-2021.csv")
+paco2021 = pandapaco.read_csv("data/paco-drawings-2021.csv")
 digital = paco2021.loc[paco2021['Medium'] == 'Digital'].count()[0]
 traditional = paco2021.loc[paco2021['Medium'] == 'Traditional'].count()[0]
 
