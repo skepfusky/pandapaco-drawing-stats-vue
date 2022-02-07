1da8ef6e2c824b30ec88187b3b445685bf8472a7
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-02-07 21:17:37 +0800

    Squashing the final results, kinda
    lmao can't be bothered anymore
    
    This is really slow and inefficent lmao
    
    I think I'm done for today lol
    
    FINALLY GETTING THE RESULTS I WANTED YESS

--- data/paco-dataset-merge-data.py
+++ data/paco-dataset-merge-data.py
@@ -3,15 +3,14 @@ import pandas as pandapaco
 df = pandapaco.read_csv('data/csv/paco-2021-updated.csv')
 
 # This code is SO BAD I can't be bothered to optimize it lol
-
 def speciesToTxt():
   species_array = df.drop(columns=['Date', 'Name', 'Character(s)', 'Expression(s)', 'Type', 'Source'])
   species_array.to_numpy()
 
-  pp = 'data/merged/species_merged.txt'
+  pp = 'data/merged/species_merged.csv'
   
   with open(pp, 'w') as f:
-    bruh = species_array.to_string(header=False, index=False)
+    bruh = species_array.to_string(index=False)
     f.write(bruh)
 
   with open(pp, 'r') as f:
@@ -21,34 +20,30 @@ def speciesToTxt():
     f.write(kiddieland)
 
   with open(pp, 'r') as f:
-    kiddieland = f.read().replace('\n', ',') # Replaces whitespace with a comma
+    kiddieland = f.read().replace('\n', ',') # Replaces line break with a comma
 
   with open(pp, 'w') as f:
     f.write(kiddieland)
 
   with open(pp, 'r') as f:
-    kiddieland = f.read().replace(',', '\n') # vice versa
+    kiddieland = f.read().replace(',', '\n') # and vice versa
 
   with open(pp, 'w') as f:
     f.write(kiddieland)
-    
-  with open(pp, 'r') as f:
-    kiddieland = f.read().replace('�', '') # Removes this character that causes pandas to get a Unicode error or some gay shit
-    
+  
   with open(pp, 'w') as f:
     f.write(kiddieland)
     
-  print('Dookied out ' + pp + 'file lol')
+  print('Dookied out ' + pp + 'file')
 
 def expressionsToTxt():
-  expressions_array = df.drop(
-      columns=['Date', 'Name', 'Character(s)', 'Species', 'Type', 'Source'])
+  expressions_array = df.drop(columns=['Date', 'Name', 'Character(s)', 'Species', 'Type', 'Source'])
   expressions_array.to_numpy()
 
-  pp = 'data/merged/expressions_merged.txt'
+  pp = 'data/merged/expressions_merged.csv'
 
   with open(pp, 'w') as f:
-    bruh = expressions_array.to_string(header=False, index=False)
+    bruh = expressions_array.to_string(index=False)
     f.write(bruh)
 
   with open(pp, 'r') as f:
